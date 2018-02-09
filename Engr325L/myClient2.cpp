650587643a973b5c0c4ab2eaa94596faba88e2af
Daniel Weber <33916231+djw33@users.noreply.github.com>
2018-02-09 15:29:39 -0500

    2/9/2018
    Worked on getting client to run from visual studio

--- Engr325L/myClient2.cpp
+++ Engr325L/myClient2.cpp
@@ -12,47 +12,17 @@ This code is compiled and run on the Macbook laptop as follows:
 */
 #include <stdio.h>
 #include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <netdb.h> 
-#include <netdb.h>
+#include <WinSock2.h>
+//#include <sys/socket.h>
+//#include <netinet/in.h>
+//#include <netdb.h>
 #include <string.h>
+#include <io.h>
 #include <stdlib.h>
-#include <unistd.h>
+//#include <unistd.h>
 #include <errno.h>
-#include <arpa/inet.h>
-#include <ncurses.h> 
+//#include <arpa/inet.h>
 
-#define UP 'A'
-#define DOWN 'B'
-#define LEFT 'D'
-#define RIGHT 'C'
-
-int getKey() {
-	int keypress = 0;
-	while(1) {
-        keypress=getch();
-        switch(keypress) {
-            case UP:
-                return 1;
-                break;
-            case DOWN:
-                return 2;
-                break;
-            case RIGHT:
-                return 3;
-                break;
-            case LEFT:
-                return 4;
-                break;
-            case -1:
-            	break;
-            default:
-            	break;
-        }
-    }
-    return 1;
-}
 
 void error(char *msg) {
     perror(msg);
@@ -101,29 +71,14 @@ int main(int argc, char *argv[])
         error( const_cast<char *>("ERROR, no such host\n") );
     
     // connect to server on portno
-    bzero( (char *) &serv_addr, sizeof(serv_addr));
+    memset( (char *) &serv_addr,0, sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
-    bcopy( (char *)server->h_addr, (char *)&serv_addr.sin_addr.s_addr, server->h_length);
+    memcpy((char *)&serv_addr.sin_addr.s_addr, (char *)server->h_addr, server->h_length);
     serv_addr.sin_port = htons(portno);
     if ( connect(sockfd,(struct sockaddr *)&serv_addr,sizeof(serv_addr)) < 0) 
         error( const_cast<char *>( "ERROR connecting") );
 
-	initscr();
-    while(1) {
-    	// get input from user
-    	n = getKey();
-    	//bzero(buffer,256);
-    	//fgets(buffer,255,stdin);
-    	//n = atoi(&ch);
-    	
-    	// send data
-    	sendData( sockfd, n );
-    	
-    	// get data
-      	//data = getData( sockfd );
-      	if(n < 0)
-      		break;
-    }
+	sendData(sockfd, n);	
 
     close( sockfd );
     return 0;
