0f5392b3a5cb3aed42912d150a6977cd0d21b575
evanheet <36342603+evanheet@users.noreply.github.com>
2018-02-28 14:09:53 -0500

    Updated 2/28/2018
    User clicks two targets. Program will calculate spacial points and vectors of the object represented by the two targets

--- ComputerVision.cpp
+++ ComputerVision.cpp
@@ -1,7 +1,8 @@
 /**
-@file ComputerVision.cpp
-February 2
+@file project1.cpp
 */
+
+// github.com/djw33/sebas
 
 #include <opencv2/opencv.hpp>
 #include <iostream>
@@ -22,35 +23,48 @@ int Hue2 = 0;
 int Saturation2 = 0;
 int Value2 = 0;
 
-int heading = 0;
-int counter = 0;
-
 vector<vector<Point> > contours1;
 vector<Vec4i> hierarchy1;
 Point2f center1;
-Point2f oldCenter1;
 float radius1;
 const Scalar& color1 = (0, 255, 0);
 
 vector<vector<Point> > contours2;
 vector<Vec4i> hierarchy2;
 Point2f center2;
-Point2f oldCenter2;
 float radius2;
 const Scalar& color2 = (255, 0, 0);
 
-// **** SET DATA SAMPLING RATE ****
+// **** SET DISTANCE BETWEEN LEDS (CM) ****
+float ledDistance = 2;
+// ****************************************
+float pixels_to_cm;
+
+// ***** SET DATA SAMPLING RATE (PER SECOND) ******
 double samplingRate = 3;
+// ***********************************
+
 double samplingInterval = (double)1 / samplingRate;
 
 clock_t lastTime;
 clock_t currentTime;
 
-double newVelocity;
-double oldVelocity;
-double acceleration;
+int heading;
+
+float vehicleX;
+float vehicleY;
+
+float oldVehicleX;
+float oldVehicleY;
+
+float deltaX;
+float deltaY;
+
+double velocityX;
+double velocityY;
 
 void CallBackFunc(int event, int x, int y, int flags, void* userdata) {
+	// Left click to indicate front of the vehicle
 	if (event == EVENT_LBUTTONDOWN) {
 		Mat imgTemp;
 		bool bSuccess = cap.read(imgTemp); // read a new frame from video
@@ -66,6 +80,8 @@ void CallBackFunc(int event, int x, int y, int flags, void* userdata) {
 		Saturation1 = hsv.val[1];
 		Value1 = hsv.val[2];
 	}
+
+	// Right click to indicate back of the vehicle
 	if (event == EVENT_RBUTTONDOWN) {
 		Mat imgTemp;
 		bool bSuccess = cap.read(imgTemp); // read a new frame from video
@@ -75,7 +91,6 @@ void CallBackFunc(int event, int x, int y, int flags, void* userdata) {
 		}
 
 		cvtColor(imgTemp, imgTemp, COLOR_BGR2HSV);
-
 		Vec3b hsv = imgTemp.at<Vec3b>(y, x);
 		Hue2 = hsv.val[0];
 		Saturation2 = hsv.val[1];
@@ -84,19 +99,26 @@ void CallBackFunc(int event, int x, int y, int flags, void* userdata) {
 }
 
 int main(int argc, char** argv) {
-	namedWindow("Control", CV_WINDOW_AUTOSIZE); //create a window called "Control"
-
-	if (!cap.isOpened())  // if not success, exit program
-	{
+	if (!cap.isOpened())  {
 		cout << "Cannot open the web cam" << endl;
 		return -1;
 	}
 	
 	lastTime = clock();
-	oldVelocity = (double)0;
 
-	oldCenter1 = Point2f(0, 0);
-	oldCenter2 = Point2f(0, 0);
+	heading = 0;
+
+	vehicleX = 0;
+	vehicleY = 0;
+
+	oldVehicleX = 0;
+	oldVehicleY = 0;
+
+	deltaX = 0;
+	deltaY = 0;
+
+	velocityX = 0;
+	velocityY = 0;
 
 	int iLowH1 = 0;
 	int iHighH1 = 179;
@@ -122,28 +144,19 @@ int main(int argc, char** argv) {
 	int iHighV2 = 255;
 	int Verror2 = 50;
 
-	//Create trackbars in "Control" window
-	cvCreateTrackbar("Hue1", "Control", &Herror1, 179); //Hue (0 - 179)
-	cvCreateTrackbar("Saturation1", "Control", &Serror1, 255); //Saturation (0 - 255)
-	cvCreateTrackbar("Value1", "Control", &Verror1, 255); //Value (0 - 255)
-	cvCreateTrackbar("Hue2", "Control", &Herror2, 179); //Hue (0 - 179)
-	cvCreateTrackbar("Saturation2", "Control", &Serror2, 255); //Saturation (0 - 255)
-	cvCreateTrackbar("Value2", "Control", &Verror2, 255); //Value (0 - 255)
-
 	while (true) {
-
 		Mat imgOriginal;
 		Mat imgHSV;
 
 		bool bSuccess = cap.read(imgOriginal); // read a new frame from video
 
-		if (!bSuccess) //if not success, break loop
-		{
+		if (!bSuccess) {
 			cout << "Cannot read a frame from video stream" << endl;
 			break;
 		}
 
-		cvtColor(imgOriginal, imgHSV, COLOR_BGR2HSV); //Convert the captured frame from BGR to HSV
+		//Convert the captured frame from BGR to HSV
+		cvtColor(imgOriginal, imgHSV, COLOR_BGR2HSV);
 
 		Mat imgThresholded1;
 		Mat imgThresholded2;
@@ -166,9 +179,11 @@ int main(int argc, char** argv) {
 		iLowV2 = Value2 - Verror2;
 		iHighV2 = Value2 + Verror2;
 
-		inRange(imgHSV, Scalar(iLowH1, iLowS1, iLowV1), Scalar(iHighH1, iHighS1, iHighV1), imgThresholded1); //Threshold the image
+		// Threshold the image
+		inRange(imgHSV, Scalar(iLowH1, iLowS1, iLowV1), Scalar(iHighH1, iHighS1, iHighV1), imgThresholded1);
 		inRange(imgHSV, Scalar(iLowH2, iLowS2, iLowV2), Scalar(iHighH2, iHighS2, iHighV2), imgThresholded2);
 
+		// Dilate the image
 		dilate(imgThresholded1, imgThresholded1, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
 		dilate(imgThresholded2, imgThresholded2, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
 
@@ -200,39 +215,53 @@ int main(int argc, char** argv) {
 			circle(imgOriginal, center2, radius2, color2);
 		}
 
+		// Draw an arrowed line between the two contours
 		if (contours2.size() != 0 & contours1.size() != 0) {
 			arrowedLine(imgOriginal, center2, center1, color1);
 
 			// If pointed NE
 			if (center1.y < center2.y && center1.x > center2.x) {
-				heading = 90 - (atan((center2.y - center1.y) / (center1.x - center2.x)) * 180 / 3.1415);
+				heading = (atan((center2.y - center1.y) / (center1.x - center2.x)) * 180 / 3.1415);
 			}
 			// If pointed NW
 			if (center1.y < center2.y && center1.x < center2.x) {
-				heading = 270 + (atan((center2.y - center1.y) / -(center1.x - center2.x)) * 180 / 3.1415);
+				heading = 180 - (atan((center2.y - center1.y) / (center2.x - center1.x)) * 180 / 3.1415);
 			}
 			// If pointed SW
 			if (center1.y > center2.y && center1.x < center2.x) {
-				heading = 180 + (atan((center2.x - center1.x) / (center1.y - center2.y)) * 180 / 3.1415);
+				heading = 180 + (atan((center1.y - center2.y) / (center2.x - center1.x)) * 180 / 3.1415);
 			}
 			// If pointed SE
 			if (center1.y > center2.y && center1.x > center2.x) {
-				heading = 180 - (atan((center1.x - center2.x) / (center1.y - center2.y)) * 180 / 3.1415);
+				heading = 360 - (atan((center1.y - center2.y) / (center1.x - center2.x)) * 180 / 3.1415);
 			}
 		}
+		
+		// Get cm/pixel ratio
+		pixels_to_cm = ledDistance / sqrt(pow(center1.x - center2.x, 2)+pow(center1.y - center2.y, 2));
 
-		// Time data is sampled
+		// Sample the clock value
 		currentTime = clock();
 
-		// Calculate velocity and acceleration based on position and time
+		// Calculate position and velocity vectors if the sampling period has passed
 		if (currentTime - lastTime >= samplingInterval * CLOCKS_PER_SEC) {
-			newVelocity = (sqrt(pow(center1.x - oldCenter1.x, 2) + pow(center1.y - oldCenter1.y, 2)))/ (((double)currentTime - (double)lastTime) / CLOCKS_PER_SEC);
-			acceleration = (newVelocity - oldVelocity)/(((double)currentTime - (double)lastTime)/CLOCKS_PER_SEC);
-			cout << "Heading: " << heading << "   " << "Velocity :" << newVelocity << "   " << "Acceleration: " << acceleration << endl;
-			oldVelocity = newVelocity;
+			vehicleX = ((center1.x + center2.x) / 2) * pixels_to_cm;
+			vehicleY = ((center1.y + center2.y) / 2) * pixels_to_cm;
+
+			deltaX = vehicleX - oldVehicleX;
+			deltaY = -(vehicleY - oldVehicleY);
+
+			velocityX = (deltaX)/(((double)currentTime - (double)lastTime) / CLOCKS_PER_SEC);
+			velocityY = (deltaY)/(((double)currentTime - (double)lastTime) / CLOCKS_PER_SEC);
+
+			/*
+			PUT CODE HERE TO OUTPUT POSITION, VELOCITY, AND HEADING VALUES USING COUT
+			OR SEND TO RASPBERRY PI
+			*/
+
 			lastTime = currentTime;
-			oldCenter1 = center1;
-			oldCenter2 = center2;
+			oldVehicleX = vehicleX;
+			oldVehicleY = vehicleY;
 		}
 
 		setMouseCallback("Original", CallBackFunc, NULL);
@@ -240,8 +269,8 @@ int main(int argc, char** argv) {
 		imshow("Thresholded Image2", imgThresholded2);
 		imshow("Original", imgOriginal); //show the original image
 
-		if (waitKey(30) == 27) //wait for 'esc' key press for 30ms. If 'esc' key is pressed, break loop
-		{
+		// Break the loop if the escape key is pressed
+		if (waitKey(30) == 27) {
 			cout << "esc key is pressed by user" << endl;
 			break;
 		}
